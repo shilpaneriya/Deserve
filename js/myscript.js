cea21ebdee678861e851539168da732228da826f
Shilpa <shilpa.neriya@gmail.com>
2018-03-29 18:07:01 +0530

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,27 @@
+/**
+ * Created by KS105 on 15-Dec-15.
+ */
+
+$(document).ready(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel();
+
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+
+    // Enable Carousel Controls
+    //$(".left").click(function(){
+        //$("#myCarousel").carousel("prev");
+    //});
+    //$(".right").click(function(){
+       // $("#myCarousel").carousel("next");
+    //});
+});
